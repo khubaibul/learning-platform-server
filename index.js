dc5856d167502c9e1d834d75feccab585fbe5de8
Khubaibul Islam <khubaibulislam2001@gmail.com>
2022-10-24 22:58:43 +0600

    res.send for all courses & one single course after find are done

--- index.js
+++ index.js
@@ -5,8 +5,20 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const allCourses = require("./data/jsonData/course/course.json");
+
 app.get("/", (req, res) => {
     res.send("CSE FROM HOME SERVER IS RUNNING....")
+})
+
+app.get("/courses", (req, res) => {
+    res.send(allCourses);
+})
+app.get("/course/:id", (req, res) => {
+    const id = req.params.id;
+
+    const requestedCourse = allCourses.find(course => course.courseId == id);
+    res.send(requestedCourse);
 })
 
 app.listen(port, () => {
